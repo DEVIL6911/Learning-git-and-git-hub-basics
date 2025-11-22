8d429b94a02092810c61f278e6fbb6fcd1cd4667
Ayush Gupta ji <guptaop34495@gmail.com>
2025-11-22 16:29:37 +0530

    34

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
+# we are using OS module to get directories and files
 
 
 print(os.listdir())
+<<<<<<< HEAD
 print(os.get_blocking())
 
 
 
 print(os.chdir("/"))  # change the current working directory to root
+=======
+
+print(os.getcwd())
+>>>>>>> 3fb27ca (done)
